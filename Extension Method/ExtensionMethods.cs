62b17e26b03e740aa277b279aa074fdb0805109e
Roshan Poudel <rpoudel912@gmail.com>
2023-02-27 20:08:36 -0700

    initial commit

---
+++ Extension Method/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Method
+{
+    internal static class ExtensionMethods
+    {
+        public static int GetWordCount(this string str) => str.Split(' ').Length;
+
+    }
+}
---
+++ Extension Method/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Extension_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Welcome to Advanced C# class";
+
+            int wordCount = myString.GetWordCount();
+
+            Console.WriteLine($"String value: {myString}");
+            Console.WriteLine($"Word count: {wordCount}");
+        }
+    }
+}
